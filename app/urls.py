82dc9b7f55a2a3517666a0b41e4a9d75aeeba0f4
saurabh wadekar <saurabhwadekar420@gmail.com>
2021-09-26 21:23:43 +0530

    2nd day

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.home),
+    path("add-book/",views.add_book,name="addbook"),
     path("create-aucount/",views.create_aucount,name="createaucont"),
     path("login-aucount/",views.login_aucount,name="createaucont"),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from .models import Book
+from .forms import UserLoginForm,UserMakeForm,BookForm
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>This is home page</h1>")
+    data = Book.objects.all()
+    dict_data = {
+        "data":data,
+    }
+    return render(request,"home.html",dict_data)
+
+def add_book(request):
+    bf = BookForm()
+    dict_data = {
+        "bf":bf
+    }
+    return render(request,"add_book.html",dict_data)
 
 def create_aucount(request):
     return render(request,"create_acount.html")
